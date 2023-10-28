5125a1432bc2c557027006cbc9eabe853f12f575
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-28 12:31:37 -0400

    first commit

---
+++ src/config/firebase.ts
@@ -0,0 +1,5 @@
+export const API_KEY = process.env.API_KEY ?? '';
+export const PROJECT_ID = process.env.PROJECT_ID ?? '';
+export const MESSAGING_SENDER_ID = process.env.MESSAGING_SENDER_ID ?? '';
+export const APP_ID = process.env.APP_ID ?? '';
+export const MEASUREMENT_ID = process.env.MEASUREMENT_ID ?? '';
