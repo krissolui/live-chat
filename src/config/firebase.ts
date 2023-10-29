49004f50f7d6cadc14f91b19420cbaedf63bf779
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-29 00:40:38 -0400

    updated firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,5 @@
-export const API_KEY = process.env.API_KEY ?? '';
-export const PROJECT_ID = process.env.PROJECT_ID ?? '';
-export const MESSAGING_SENDER_ID = process.env.MESSAGING_SENDER_ID ?? '';
-export const APP_ID = process.env.APP_ID ?? '';
-export const MEASUREMENT_ID = process.env.MEASUREMENT_ID ?? '';
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY ?? '';
+export const PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID ?? '';
+export const MESSAGING_SENDER_ID = process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID ?? '';
+export const APP_ID = process.env.NEXT_PUBLIC_APP_ID ?? '';
+export const MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID ?? '';
---
+++ src/config/firestore.ts
@@ -0,0 +1,3 @@
+export const USER_COLLECTION = 'users';
+export const ROOM_COLLECTION = 'rooms';
+export const MESSAGE_COLLECTION = 'messages';
