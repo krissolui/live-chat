0f32ecf25b9276677cb7b21625873ed172b5c525
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-29 14:40:40 -0400

    added listen users function

--- src/firebase/users.ts
+++ src/firebase/users.ts
@@ -1,17 +1,18 @@
-import { getFirestore, collection, getDocs, doc, setDoc, getDoc } from 'firebase/firestore';
+import { getFirestore, collection, getDocs, doc, setDoc, getDoc, onSnapshot } from 'firebase/firestore';
 import { UserProfile } from '@/types/user';
 import { USER_COLLECTION } from '@/config/firestore';
 import { app } from './firebase';
 import { converter } from './converter';
 
 const db = getFirestore(app);
+const userCollection = collection(db, USER_COLLECTION).withConverter(converter<UserProfile>());
 
 export const createUserProfile = async (user: UserProfile): Promise<void> => {
     if (await isUserExists(user.id)) return;
 
     try {
         await setDoc(doc(db, USER_COLLECTION, user.id), user);
-        console.log('Document written with ID: ', user.id);
+        console.log('create user profile success');
     } catch (err) {
         console.error('create user profile error', err);
     }
@@ -19,7 +20,7 @@ export const createUserProfile = async (user: UserProfile): Promise<void> => {
 
 export const getUsers = async (): Promise<UserProfile[]> => {
     try {
-        const querySnapshot = await getDocs(collection(db, USER_COLLECTION).withConverter(converter<UserProfile>()));
+        const querySnapshot = await getDocs(userCollection);
         return querySnapshot.docs.map((doc) => {
             return doc.data();
         });
@@ -33,4 +34,15 @@ const isUserExists = async (id: string): Promise<boolean> => {
     const docRef = doc(db, USER_COLLECTION, id).withConverter(converter<UserProfile>());
     const docSnap = await getDoc(docRef);
     return docSnap.exists();
+};
+
+export const listenUsers = (callback: (users: UserProfile[]) => void) => {
+    return onSnapshot(userCollection, (querySnapshot) => {
+        const users = querySnapshot.docs.map((doc) => {
+            const data = doc.data();
+            return { ...data, id: doc.id };
+        });
+
+        callback(users);
+    });
 };
